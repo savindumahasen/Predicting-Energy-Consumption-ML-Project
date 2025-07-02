fe21a742b825935a0d733b7e426f46849994dddd
Savindu <savinduruhunuhewa@gmail.com>
2025-07-02 16:24:38 +0530

    Setup the react front end

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Energy Consuming Prediction App</h1>
+     <h3>How are you</h3>
+    </div>
+  );
+}
+
+export default App;
