9d3b0675799892915062723bd09e619e44d2e4a4
Savindu <savinduruhunuhewa@gmail.com>
2025-07-03 07:17:37 +0530

    Setup the reac+vite app with tailwind css

---
+++ front-end/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   
+    <div className='text-2xl font-bold underline'>
+         <div className='flex justify-center'>
+         <div className="margin-top:200px">Consumping Energy Prediction App</div>
+        </div>
+    </div>
+  )
+}
+
+export default App
