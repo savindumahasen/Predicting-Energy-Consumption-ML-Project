a8831e191ad5734ef75552e267f0e62ae3df2a66
Savindu <savinduruhunuhewa@gmail.com>
2025-07-03 08:52:10 +0530

    create the new route called /predict

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ scaling=pickle.load(open('scaler.pkl','rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data=request.json['data']
     data = np.array(list(data.values())).reshape(1,-1)
@@ -26,6 +26,15 @@ def predict_api():
     return jsonify(predictions[0])
 
 
+@app.route('/predict', methods=["POST"])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    print(data)
+    final_input=scaling.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    predictions=regression_model.predict(final_input)[0]
+    #return jsonify({'Result':predictions})
+    return render_template('index.html',prediction_text="The energy consuming prediction is  {}".format(predictions))
 if __name__=="__main__":
     app.run(debug=True, port=5000)
 
